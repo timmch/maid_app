8d22703490e1fa8a9944eb9371b626d553233458
Tim McHugh <timmch@gmail.com>
2012-06-27 17:41:18 -0400

    Created index view for employees, had to edited settins.py as well to set temporary directory to the maid_app directory

--- backend/Employees/views.py
+++ backend/Employees/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+from Employees.models import Employee, EmployeeForm
+from django.template import RequestContext
+from django.shortcuts import render_to_response, redirect
+from django.forms.models import modelformset_factory
+
+def ip_address_processor(request):
+    return {'ip_address': request.META['REMOTE_ADDR']}
+
+def index(request):
+    all_employees = Employee.objects.all()
+    t = loader.get_template('employees/view_employees.html')
+    c = RequestContext(request, {
+        'all_employees': all_employees,
+	}, [ip_address_processor])
+    return HttpResponse(t.render(c))
