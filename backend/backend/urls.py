1ce8bd212a3b291d1099b42f673beb9e08af8bc0
Tim McHugh <timmch@gmail.com>
2012-07-03 15:52:59 -0400

    Made to do list in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-	url(r'^Employees/$', 'Employees.views.index'),
+	url(r'^Employees/$', 'Employees.views.index'), #View all employees
+	url(r'^Employees/(\d{1,3})/$', 'Employees.views.index'), #To do - for viewing an individual employee by there id number
+	url(r'^Clients/$', 'Employees.views.index'), #View all employees
+	url(r'^Clients/(\d{1,3})/$', 'Employees.views.index'), #To do - for viewing an individual client by there id number
 	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^admin/', include(admin.site.urls)),
 )
