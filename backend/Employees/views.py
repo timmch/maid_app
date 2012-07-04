065daf659ec755fe913d3fd210cdb6f3cbc5819c
Tim McHugh <timmch@gmail.com>
2012-07-04 14:42:10 -0400

    Starting to extend user accounts to include fields necessary for Employees

--- backend/Employees/views.py
+++ backend/Employees/views.py
@@ -4,12 +4,14 @@ from Employees.models import Employee, EmployeeForm
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 from django.forms.models import modelformset_factory
+from django.contrib.auth.models import User, Group
 
 def ip_address_processor(request):
     return {'ip_address': request.META['REMOTE_ADDR']}
 
 def index(request):
-    all_employees = Employee.objects.all()
+    all_employees = User.objects.all()
+	#filter(groups__name='Manager') to filter by group name
     t = loader.get_template('view_employees.html')
     c = RequestContext(request, {
         'all_employees': all_employees,
@@ -23,3 +25,8 @@ def individual_employee(request, idnum):
 		raise Http404()
 	html =  "<html><body>Employee to View is employee with id #%s</body></html>" % (idnum)
 	return HttpResponse(html)
+def tester(request):
+	t = loader.get_template('tester.html')
+	c = RequestContext(request, {'foo': 'bar'})
+	return HttpResponse(t.render(c))
+
